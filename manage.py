6fdd86643b2d28f84a3c10aa52e7b1891376d183
Christian Schmitz <mail@schmizz.com>
2012-03-09 16:36:00 +0100

    Use friendly-foundation manager stuff

--- manage.py
+++ manage.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python
 import os
-
-from django.utils.importlib import import_module 
+from django.utils.importlib import import_module
 from django.core.management import execute_manager
 
-PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+# Edit this if necessary or override the variable in your environment.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 try:
-    module_name = 'project.settings'
-    module = import_module(module_name) 
-#    imp.find_module('settings') # Assumed to be in the same directory.
+    from foundation import manage
+    manage.setup_environment(__file__)
 except ImportError:
     import sys
     sys.stderr.write("Error: Can't find the file 'settings.py' in %r.\n" % PROJECT_ROOT)
     sys.exit(1)
 
-#import settings
-
 if __name__ == "__main__":
-    execute_manager(module)
+    manage.main()
