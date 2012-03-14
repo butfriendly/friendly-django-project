cc4ecd63ec6c27093ec9e66adbba7c086da2c139
Christian Schmitz <mail@schmizz.com>
2012-03-14 12:58:28 +0100

    Revampes URLs

--- project/urls.py
+++ project/urls.py
@@ -1,33 +1,30 @@
 from django.conf import settings
 from django.conf.urls.defaults import *
-
-# Uncomment the next two lines to enable the admin:
-from django.conf.urls.static import static
-from django.contrib import admin
+from foundation.urls import staticfiles_urlpatterns
 from waffle.views import wafflejs
 from project.example import urls
 
-admin.autodiscover()
+urlpatterns = []
 
-urlpatterns = patterns('',
-    # Use Waffle flags directly in JavaScript
-    url(r'^wafflejs$', wafflejs, name='wafflejs'),
-
-    # Example:
-    (r'', include(urls)),
-)
-
-# Admin URLs
+# We attach the admin URLs if we detected the admin app
 if 'django.contrib.admin' in settings.INSTALLED_APPS:
-    urlpatterns += patterns('',
+    from django.contrib import admin
+
+    admin.autodiscover()
+
+    urlpatterns = patterns('',
         (r'^admin/doc/', include('django.contrib.admindocs.urls')),
         (r'^admin/', include(admin.site.urls)),
     )
 
-## In DEBUG mode, serve media files through Django.
+# We serve media files through Django in debug mode
 if settings.DEBUG:
-    # Just serve the files within MEDIA_ROOT
-    urlpatterns += static(settings.MEDIA_URL, view='django.views.static.serve', document_root=settings.MEDIA_ROOT)
+    urlpatterns += staticfiles_urlpatterns()
 
-    # Same as above, but also staticfiles finders are used to find statics
-    urlpatterns += static(settings.STATIC_URL, view='django.contrib.staticfiles.views.serve', document_root=settings.STATIC_ROOT)
+urlpatterns += patterns('',
+    # Use Waffle flags directly in JavaScript
+    url(r'^waffle.js$', wafflejs, name='wafflejs'),
+
+    # Example:
+    (r'', include(urls)),
+)
